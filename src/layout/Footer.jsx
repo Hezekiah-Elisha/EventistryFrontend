46c0799400ea672cc9f55f05c7b307d0e3066523
Hezekiah Elisha <elishahezekiah903@gmail.com>
2024-11-02 12:01:58 +0300

    feat: update footer text to include 'Coming Soon' message

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-primary text-white p-5 text-center">
-      &copy; 2024 Eventistry. All rights reserved.
+      &copy; 2024 Eventistry. All rights reserved. Coming Soon
     </footer>
   );
 }
